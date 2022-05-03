608436de6c688a775af12d7eeea705cd9fc03dc3
jloperad <51926858+jloperad@users.noreply.github.com>
2022-05-03 18:45:09 -0500

    eslint config

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,55 +1,55 @@
-const agent = require("superagent");
-const statusCode = require("http-status-codes");
-const chai = require("chai");
+const agent = require('superagent');
+const statusCode = require('http-status-codes');
+const chai = require('chai');
 
-const expect = chai.expect;
+const { expect } = chai;
 
-describe("First Api Tests", () => {
-  it("Consume GET Service", async () => {
-    const response = await agent.get("https://httpbin.org/ip");
+describe('First Api Tests', () => {
+  it('Consume GET Service', async () => {
+    const response = await agent.get('https://httpbin.org/ip');
 
     expect(response.status).to.equal(statusCode.OK);
-    expect(response.body).to.have.property("origin");
+    expect(response.body).to.have.property('origin');
   });
 
-  it("Consume GET Service with query parameters", async () => {
+  it('Consume GET Service with query parameters', async () => {
     const query = {
-      name: "John",
-      age: "31",
-      city: "New York",
+      name: 'John',
+      age: '31',
+      city: 'New York'
     };
 
-    const response = await agent.get("https://httpbin.org/get").query(query);
+    const response = await agent.get('https://httpbin.org/get').query(query);
 
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body.args).to.eql(query);
   });
 
-  it("Consume POST Service with query parameters", async () => {
+  it('Consume POST Service with query parameters', async () => {
     const body = {
-      name: "Tony",
-      age: "61",
-      city: "Habana",
-      job: "Developer",
+      name: 'Tony',
+      age: '61',
+      city: 'Habana',
+      job: 'Developer'
     };
 
-    const response = await agent.post("https://httpbin.org/post").send(body);
+    const response = await agent.post('https://httpbin.org/post').send(body);
 
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body.json).to.eql(body);
   });
 
-  it("Consume HEAD Service", async () => {
-    const response = await agent.head("https://httpbin.org/ip");
+  it('Consume HEAD Service', async () => {
+    const response = await agent.head('https://httpbin.org/ip');
 
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body).to.be.empty;
   });
 
-  it("Consume PATCH Service", async () => {
-    const response = await agent.patch("https://httpbin.org/anything");
+  it('Consume PATCH Service', async () => {
+    const response = await agent.patch('https://httpbin.org/anything');
 
     expect(response.status).to.equal(statusCode.OK);
-    expect(response.body).to.have.property("origin");
+    expect(response.body).to.have.property('origin');
   });
 });
