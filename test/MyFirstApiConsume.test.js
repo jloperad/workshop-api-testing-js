125d8649c1c440c1d7c3ad2304dd92e1172b1c22
jloperad <51926858+jloperad@users.noreply.github.com>
2022-05-03 17:12:29 -0500

    PUT,HEAD & PATCH services added

---
+++ test/MyFirstApiConsume.test.js
@@ -0,0 +1,55 @@
+const agent = require("superagent");
+const statusCode = require("http-status-codes");
+const chai = require("chai");
+
+const expect = chai.expect;
+
+describe("First Api Tests", () => {
+  it("Consume GET Service", async () => {
+    const response = await agent.get("https://httpbin.org/ip");
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property("origin");
+  });
+
+  it("Consume GET Service with query parameters", async () => {
+    const query = {
+      name: "John",
+      age: "31",
+      city: "New York",
+    };
+
+    const response = await agent.get("https://httpbin.org/get").query(query);
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.args).to.eql(query);
+  });
+
+  it("Consume POST Service with query parameters", async () => {
+    const body = {
+      name: "Tony",
+      age: "61",
+      city: "Habana",
+      job: "Developer",
+    };
+
+    const response = await agent.post("https://httpbin.org/post").send(body);
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body.json).to.eql(body);
+  });
+
+  it("Consume HEAD Service", async () => {
+    const response = await agent.head("https://httpbin.org/ip");
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.be.empty;
+  });
+
+  it("Consume PATCH Service", async () => {
+    const response = await agent.patch("https://httpbin.org/anything");
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property("origin");
+  });
+});
